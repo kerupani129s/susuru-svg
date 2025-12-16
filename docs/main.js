60bb629b56fcd9f0ca545b3a2cab457aced570a9
ケルパニ＠猫 <32389989+kerupani129s@users.noreply.github.com>
2025-12-16 20:59:47 +0900

    Update: Canvas API で描画する SVG ファイルのパスをデータ属性で扱うように変更

--- docs/main.js
+++ docs/main.js
@@ -4,7 +4,7 @@
 	const context = canvas.getContext('2d');
 
 	const image = new Image();
-	image.src = 'susuru.svg';
+	image.src = canvas.dataset.src;
 	await image.decode();
 
 	context.drawImage(image, 0, 0);
